4db103264036055961ce3b85cb311493ca249b26
khadijah-git <80583817+khadijah-git@users.noreply.github.com>
2021-06-07 16:10:52 +0500

    Add files via upload

---
+++ Program 9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            Console.ReadLine();
+        }
+    }
+}
