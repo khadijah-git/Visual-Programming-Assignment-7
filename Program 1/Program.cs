19fd05b6774374cad164b799d70d9e03e6f0e3d9
khadijah-git <80583817+khadijah-git@users.noreply.github.com>
2021-06-07 15:54:33 +0500

    Add files via upload

---
+++ Program 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_1
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public void Introduce()
+        {
+            Console.WriteLine("Hello ! , My name is " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Thomas = new Person();
+            Thomas.firstName = " Thomas";
+            Thomas.lastName = " Newt";
+            Thomas.Introduce();
+        }
+    }
+}
